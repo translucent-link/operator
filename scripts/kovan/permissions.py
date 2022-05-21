fb291c6ce25783fb35ca19fdf0f10da196cd13b0
Ijonas Kisselbach <ijonas@ijonas.com>
2022-05-21 11:35:02 +0100

    fixes to Kovan node address

--- scripts/kovan/permissions.py
+++ scripts/kovan/permissions.py
@@ -4,6 +4,6 @@ from web3 import Web3
 
 def main():
     acct = accounts.load('deployer')
-    operator = Operator.at("0x188b71C9d27cDeE01B9b0dfF5C1aff62E8D6F434")
+    operator = Operator.at("0xf71775b3640D7034466e0321E35c5CFB78fd212F")
     operator.setAuthorizedSenders(
         ["0xfAec569aF320661E150ae1A2a669F2A04aB9Fd3A"], {'from': acct})
--- scripts/kovan/verify.py
+++ scripts/kovan/verify.py
@@ -4,5 +4,5 @@ from brownie import Operator
 
 def main():
     operator = Operator.at(
-        "0x188b71C9d27cDeE01B9b0dfF5C1aff62E8D6F434")
+        "0xf71775b3640D7034466e0321E35c5CFB78fd212F")
     Operator.publish_source(operator)
