2453774c6da2793c98b855de2bd797cfb1c6856a
Ijonas Kisselbach <ijonas@ijonas.com>
2022-05-21 17:19:15 +0100

    permissions for new Polygon node

--- scripts/polygon/permissions.py
+++ scripts/polygon/permissions.py
@@ -7,4 +7,4 @@ def main():
 
     operator = Operator.at("0x188b71C9d27cDeE01B9b0dfF5C1aff62E8D6F434")
     operator.setAuthorizedSenders(
-        ["0x1Abc1de91F50c7f4179E8DE292E9f5cD2b24c434"], {'from': acct})
+        ["0x806b703FDa1c8F524D574CEb6C25C62D4442ab97"], {'from': acct})
